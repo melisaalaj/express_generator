cd76fd6a8b4406e17bed3df7f0c469c1afbb3097
Melisa Alaj <melisa.alaj.7@gmail.com>
2023-02-23 18:58:48 +0100

    Write and Read files using promises

--- promis.js
+++ promis.js
@@ -1,15 +1,38 @@
-const promis = new Promise((resolve, reject) => {
-    const i = 1;
+const fs = require('fs')
 
-    if(i===2){
-        resolve()
-    } else {
+
+const promis = new Promise((resolve, reject) => {
+
+    fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+    if(err){
         reject()
-    }
+    } else {
+        resolve()
+    } });
+
+    
+
 });
 
+const promisRead = new Promise((resolve, reject) => {
+
+    readFile('mynewfile3.txt', (err) => {
+        if(err){
+            reject()
+        } else {
+            resolve()
+        } 
+    });
+})
+
 promis.then(function () {
-    console.log('Pass')
+    console.log('File written successfully!')
 }).catch(function() {
-    console.log('Error')
+    console.log('Could not write file')
+})
+
+promisRead.then(function () {
+    console.log('File readen successfully!')
+}).catch(function() {
+    console.log('Could not read file')
 })
