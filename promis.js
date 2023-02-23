77c44e0eb51fd319dd5f9dc2aa131a9fc14e8ea7
Melisa Alaj <melisa.alaj.7@gmail.com>
2023-02-23 19:36:57 +0100

    readdir using premises

--- promis.js
+++ promis.js
@@ -3,35 +3,20 @@ const fs = require('fs')
 
 const promis = new Promise((resolve, reject) => {
 
-    fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-    if(err){
-        reject()
-    } else {
-        resolve()
-    } });
+    fs.readdir(__dirname, (err, files) => {
+        if (err)
+          reject()
+        else {
+          resolve(files)
+        }
+      })
 
 });
 
-const promisRead = new Promise((resolve, reject) => {
 
-    fs.readFile('mynewfile3.txt', (err) => {
-        if(err){
-            reject()
-        } else {
-            resolve()
-        } 
-    });
-
-})
-
-promis.then(function () {
-    console.log('File written successfully!')
+promis.then((files) => {
+        console.log(files);
 }).catch(function() {
-    console.log('Could not write file')
+    console.log('Could not read directory!')
 })
 
-promisRead.then(function () {
-    console.log('File read successfully!')
-}).catch(function() {
-    console.log('Could not read file')
-})
